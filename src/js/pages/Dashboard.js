2b1906e14faf23546a035ba6e15457111e1b4c82
Jack Fay <JF046975@cerner.net>
2017-10-20 14:39:23 -0500

    changed batch id to convert to all upper case

--- src/js/pages/Dashboard.js
+++ src/js/pages/Dashboard.js
@@ -64,7 +64,7 @@ export default class Dashboard extends React.Component{
       var xmlString = generateXML(this.state.csvData, this.state.batchId);
       var xml = StringToXML(xmlString);
       var serializedXml = new XMLSerializer().serializeToString(xml);
-      this.props.dispatch(postTransactions(serializedXml, this.state.apiKey, this.state.batchId));
+      this.props.dispatch(postTransactions(serializedXml, this.state.apiKey, this.state.batchId.toUpperCase()));
     }
 
     postBatchMetaData(error_body){
